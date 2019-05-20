bf496bc5582fb223e1df98dac94ee8fb7aeb7a63
Mostafa Elsheikh <mostafa.elsheikh@mokalmat.com>
2019-05-20 15:19:03 +0200

    add test cases

--- problem_6-unsorted_integer_array.py
+++ problem_6-unsorted_integer_array.py
@@ -23,7 +23,20 @@ def get_min_max_by_sorting(ints):
 ## Example Test Case of Ten Integers
 import random
 
+# Test Case 1
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
 
 print ("Pass" if ((0, 9) == get_min_max_by_sorting(l)) else "Fail")
+
+# Test Case 2
+l = [3, 2, 5, 7, 3, 2, 1]
+random.shuffle(l)
+
+print ("Pass" if ((1, 7) == get_min_max_by_sorting(l)) else "Fail")
+
+# Test Case 3
+l = [6, 3, 5, 2, 5, 2, 5]
+random.shuffle(l)
+
+print ("Pass" if ((2, 6) == get_min_max_by_sorting(l)) else "Fail")
